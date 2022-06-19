bc31ccc53d545406914b1ac668be42dee5cd774c
Daniel Jardim <jn_dani@outlook.com>
2022-06-19 16:22:49 -0300

    Atualizando main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from classes.Musicas import Musicas
+from classes.Bandas import Bandas
+
 def divider():
     print('\n' + '-' * 80 + '\n')
 
@@ -5,12 +8,10 @@ while 1:
     option = input('1. Musicas\n2. Bandas\n3. Sair\n')
 
     if option == '1':
-        from classes.Musicas import MusicasCRUD
-        MusicasCRUD()
+        Musicas()
     
     elif option == '2':
-        from classes.Bandas import BandasCRUD
-        BandasCRUD()
+        Bandas()
     
     elif option == '3':
         break
