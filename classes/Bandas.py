f0cfeb4ba745ecbe88d99cffe4a246a21f2432b7
Daniel Jardim <jn_dani@outlook.com>
2022-06-19 16:21:29 -0300

    Atualizando Classe Bandas

--- classes/Bandas.py
+++ classes/Bandas.py
@@ -4,51 +4,52 @@ from classes.BancoDeDados import BandasCRUD
 def divider():
     print('\n' + '-' * 80 + '\n')
 
-b = BandasCRUD()
+def Bandas():
+    b = BandasCRUD()
 
-while 1:    
-    option = input('1. Create\n2. Read\n3. Update\n4. Delete\n5. Sair\n')
+    while 1:    
+        option = input('1. Create\n2. Read\n3. Update\n4. Delete\n5. Sair\n')
 
-    if option == '1':
-        nome = input('  Nome: ')
-        estilo = input('   Estilo: ')
-        autor = input('  Autor: ')
-        quantIntegrantes = input('  QuantIntegrantes: ')
-        banda = {
-            'nome': nome,
-            'estilo': estilo,
-            'autor': autor,
-            'quantIntegrantes': quantIntegrantes
-        }
-        aux = b.create(banda)
-        divider()
+        if option == '1':
+            nome = input('  Nome: ')
+            estilo = input('   Estilo: ')
+            autor = input('  Autor: ')
+            quantIntegrantes = input('  QuantIntegrantes: ')
+            banda = {
+                'nome': nome,
+                'estilo': estilo,
+                'autor': autor,
+                'quantIntegrantes': quantIntegrantes
+            }
+            aux = b.create(banda)
+            divider()
 
-    elif option == '2':
-        aux = b.read_bandas()
-        pp(aux)
-        divider()
+        elif option == '2':
+            aux = b.read_bandas()
+            pp(aux)
+            divider()
 
-    elif option == '3':
-        nome = input('  Nome: ')
-        estilo = input('   Estilo: ')
-        banda = {
-            'nome': nome,
-            'estilo': estilo
-        }
+        elif option == '3':
+            nome = input('  Nome: ')
+            estilo = input('   Estilo: ')
+            banda = {
+                'nome': nome,
+                'estilo': estilo
+            }
         
-        aux = b.update_estilo(banda)
-        divider()
+            aux = b.update_estilo(banda)
+            divider()
 
-    elif option == '4':
-        nome = input('  Nome: ')
-        banda = {
-            'nome': nome
-        }
+        elif option == '4':
+            nome = input('  Nome: ')
+            banda = {
+                'nome': nome
+            }
         
-        aux = b.delete(banda)
-        divider()
+            aux = b.delete(banda)
+            divider()
 
-    elif option == '5':
-        break
+        elif option == '5':
+            break
 
-b.db.close()
+    b.db.close()
