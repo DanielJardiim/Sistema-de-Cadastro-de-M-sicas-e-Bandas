d8b361c992667a673b8c5db310cab98363e55bc5
Daniel Jardim <jn_dani@outlook.com>
2022-06-19 21:39:41 -0300

    Atualizando Classe main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from classes.Musicas import Musicas
 from classes.Bandas import Bandas
+from classes.BancoDeDados import Relacionamentos
 
 def divider():
     print('\n' + '-' * 80 + '\n')
 
 while 1:    
-    option = input('1. Musicas\n2. Bandas\n3. Sair\n')
+    option = input('1. Musicas\n2. Bandas\n3. Relação\n4. Deletar Banco\n5. Sair\n')
 
     if option == '1':
         Musicas()
@@ -14,7 +15,23 @@ while 1:
         Bandas()
     
     elif option == '3':
+        nome1 = input("Nome da Musica: ")
+        nome2 = input("Nome da Banda: ")
+        desde = input("Desde de quando está Musica pertence a está Banda: ")
+
+        musica = {'nome': nome1}
+        banda = {'nome': nome2}
+        relacao = {'desde': desde}
+
+        aux = Relacionamentos().create_relation(musica, banda, relacao)
+        divider()
+
+    elif option == '4':
+        aux = Relacionamentos().delete_all_nodes()
+        divider()
+
+    elif option == '5':
         break
 
     else:
-        option = input('1. Musicas\n2. Bandas\n3. Sair\n')
+        option = input('1. Musicas\n2. Bandas\n3. Relação\n4. Deletar Banco\n5. Sair\n')
